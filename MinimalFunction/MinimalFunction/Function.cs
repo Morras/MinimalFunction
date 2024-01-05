a4f517661fa8199eb811bd62ddcb1a45011879fa
Morten Rasmussen <morasm@norlys.dk>
2024-01-05 11:39:24 +0100

    Minimal function with timer trigger

---
+++ MinimalFunction/MinimalFunction/Function.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace CostOverview.App.api;
+
+public class Function
+{
+    private readonly ILogger<Function> _log;
+
+    public Function(ILogger<Function> log)
+    {
+        _log = log;
+    }
+
+
+    [Function("TimerFunction")]
+    // Triggered to run at 15 and 45 mins after the hour each hour.
+    public void Run([TimerTrigger("0 15,45 * * * *", UseMonitor = true, RunOnStartup = false)] TimerInfo myTimer)
+    {
+        _log.LogInformation("Timer triggered!");
+    }
+}
---
+++ MinimalFunction/MinimalFunction/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CostOverview.App.api;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureServices(s =>
+            {
+                s.AddApplicationInsightsTelemetryWorkerService();
+                s.ConfigureFunctionsApplicationInsights();
+
+                // https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide
+                s.Configure<LoggerFilterOptions>(options =>
+                {
+                    // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
+                    // Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
+                    LoggerFilterRule toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                        == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+                    if (toRemove is not null)
+                    {
+                        options.Rules.Remove(toRemove);
+                    }
+                });
+            })
+            .Build();
+
+        await host.RunAsync();
+    }
+}
